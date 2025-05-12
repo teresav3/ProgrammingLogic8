6d1aa6cd561dabb6d1bd2f85749f81b5fb1a0e27
teresav3 <tvargin@gmail.com>
2025-05-12 02:18:54 +0000

    commit

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -1,25 +1,36 @@
-﻿/*Step 1 - Create a base class called Animal with a virtual method 
-    animalSound() that outputs: The animal makes a sound
+﻿namespace Lab8;
 
-Step 2 - Create two derived classes:
-    Cat – override animalSound() to say:The cat says: meow
-    Dog – override animalSound() to say:The dog says: bow wow
+/*Step 1 - Create a base class called Animal with a virtual method 
+    animalSound() that outputs: The animal makes a sound*/
 
-Step 3 - In the Main() method:
-    Create one object each of Animal, Cat, and Dog, 
-    but use the base class Animal as the reference type.
-
-    Call animalSound() on each.*/
-
-
-
-
-namespace Lab8;
-
-class Animal
+using System;
+public class Animal
 {
-    static void Main(string[] args)
+    public virtual void AnimalSound()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("The animal makes a sound");
     }
 }
+public class Dog : Animal
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("The dog barks");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+
+        myAnimal.AnimalSound();
+        myDog.AnimalSound();  
+    }
+}
+
+
+
+
